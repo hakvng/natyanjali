cd9444a90641d592f007b74ad73de11d052b6e59
Nandaja Varma <nandaja.varma@gmail.com>
2014-08-19 18:02:43 +0530

    Initial setup for the dance school app

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_SYNTAX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_SYNTAX }
+  validates_uniqueness_of :email, case_sensitive: false
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
