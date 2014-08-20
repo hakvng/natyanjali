6f3cb3161dc5b5b9e27cbd404a2ea29815f777e2
Nandaja Varma <nandaja.varma@gmail.com>
2014-08-20 08:08:12 +0530

    Fixing the type in permit arguments and adding flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "You have registered successfully!"
       redirect_to @user
     else
       render 'new'
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmations)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
