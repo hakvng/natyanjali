66c9b047396321db9746c8d28ff9f57c88b12f9a
Nandaja Varma <nandaja.varma@gmail.com>
2014-08-20 11:56:50 +0530

    Adding the sign in page and sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:error] = "Invalid email/password"
+      render 'new'
+    end 
+  end
+  def destroy
+  end
+end
---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Authentication" do
+  subject { page }
+  
+  describe "sign in page" do
+    before { visit signin_path }
+    
+    describe "invalid infomation must get rejected" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('div.alert.alert-error') }
+      
+      describe "after visiting the home page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    describe "valid information must get the user signed in" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+       fill_in "Email", with: user.email.upcase
+       fill_in "Password",  with: user.password
+       click_button "Sign in"
+      end
+      
+      it { should have_link("Profile", href: user_path(user)) }
+      it { should have_link("Sign out",  href: signout_path(user)) }
+      it { should_not have_link("Sign in", href: signin_path(user)) }
+    end
+
+  end
+end
