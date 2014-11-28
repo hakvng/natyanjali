869412de084fba27ec13957286fcfa0ce87551b4
gorbinphilip <gorbinphilip@gmail.com>
2014-11-28 16:43:17 +0530

    added verified flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119140259) do
+ActiveRecord::Schema.define(version: 20141128111218) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20141119140259) do
     t.string   "g_name"
     t.string   "place"
     t.boolean  "admin",                     default: false
+    t.boolean  "verified",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
