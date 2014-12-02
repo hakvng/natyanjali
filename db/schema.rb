74d683013e415e57bf8b125fad3dbec2ad20d9a7
gorbinphilip <gorbinphilip@gmail.com>
2014-12-02 13:31:44 +0530

    added enrollment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202075213) do
+ActiveRecord::Schema.define(version: 20141202075522) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20141202075213) do
     t.string   "name"
     t.decimal  "fee",        precision: 10, scale: 0
     t.integer  "period"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "enrollments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
