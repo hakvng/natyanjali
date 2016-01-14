89c7631a71dae1bbca4d81f3cc70e67f4545aaaa
Nandaja Varma <nandaja.varma@gmail.com>
2016-01-14 17:54:24 +0530

    Initial commit

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name "Nandaja Varma"
-    email "nandaja@btt.com"
-    password  "foobar"
-    password_confirmation "foobar"
-  end
-end
--- spec/factories/courses.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :course do
-    name "MyString"
-    fee "9.99"
-    period 1
-  end
-end
